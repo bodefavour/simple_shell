6483b70169db0444b09a21da2d34a75c6214b439
bodefavour <bodefavour@gmail.com>
2023-11-09 18:24:21 -0800

    fix task 3: Simple Shell 0.2+; enabling it to handle PATH,and ensuring fork must not be called if command doesn't exist

--- execute_shell-command.c
+++ execute_shell-command.c
@@ -6,6 +6,10 @@
  * @sh_in_command: command to execute
  * Return: void
 */
+
+void execute_sh_child_process(const char *sh_in_command, const char *args[]);
+void search_cm_and_execute(const char *sh_in_command, const char *args[]);
+
 void execute_sh_command(const char *sh_in_command)
 {
     pid_t child_pid = fork();
@@ -19,18 +23,16 @@ void execute_sh_command(const char *sh_in_command)
     {
 	    const char *args[] = {NULL, NULL};
 	    args[0]=sh_in_command;
-
-	    if (execve(sh_in_command, (char *const *)args, NULL) == -1)
-	    {
-		    perror("execve");
-		    exit(EXIT_FAILURE);
-	    }
+	    search_cm_and_execute(sh_in_command, args);
     }
     else
     {
         int status;
-	waitpid(child_pid, &status, 0);
-
+	if (waitpid(child_pid, &status, 0) == -1)
+	{
+		perror("waitpid");
+		exit(EXIT_FAILURE);
+	}
 	if (WIFEXITED(status))
 	{
 		printf("Child process exited with status %d\n", WEXITSTATUS(status));
@@ -40,4 +42,49 @@ void execute_sh_command(const char *sh_in_command)
 		fav_print("Child process terminated somehow abnormally\n");
 	}
     }
+}
+
+void execute_sh_child_process(const char *sh_in_command, const char *args[])
+{
+	if (execve(sh_in_command, (char *const *)args, NULL) ==-1)
+	{
+		perror("execve");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void search_cm_and_execute(const char *sh_in_command, const char *args[])
+{
+	char *path = getenv("PATH");
+	char *token, *sh_in_command_path = NULL;
+	
+	if (path != NULL)
+	{
+		path = strdup(path);
+		token = strtok(path, ":");
+		
+		while (token != NULL)
+		{
+			sh_in_command_path = malloc(strlen(token) + strlen(sh_in_command) + 2);
+			
+			if (sh_in_command_path == NULL)
+			{
+				perror("malloc");
+				exit(EXIT_FAILURE);
+			}
+			sprintf(sh_in_command_path, "%s%s", token, sh_in_command);
+			
+			if (access(sh_in_command_path, X_OK) == 0)
+			{
+				args[0] = sh_in_command_path;
+				execute_sh_child_process(sh_in_command_path, args);
+				free(sh_in_command_path);
+				token = strtok(NULL, ":");
+			}
+			free(path);
+		}
+		
+		perror("command not found");
+		exit(EXIT_FAILURE);
+	}
 }
--- s_shell.h
+++ s_shell.h
@@ -17,5 +17,7 @@ void s_h_prompt(void);
 void fav_print(const char *sh_in_message);
 void read_favour_command(char *sh_in_command, size_t size_s);
 void execute_sh_command(const char *sh_in_command);
+void execute_sh_child_process(const char *sh_in_command, const char *args[]);
+void search_cm_and_execute(const char *sh_in_command, const char *args[]);
 
 #endif /* _SHELL_H */
